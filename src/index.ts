52bce3af0437eecd2edd5d6761c036c3d80710b1
Shikhar Bansal <shikharbansal007@gmail.com>
2024-03-30 21:08:25 +0530

    Implemented git-clone function

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,14 @@ import express from "express";
 import cors from "cors";
 import { generate } from "./utils";
 import simpleGit from "simple-git";
+import bodyParser from "body-parser";
+
 
 const app = express();
 app.use(cors());
+app.use(bodyParser.json());
 
-app.post("./deploy", async (req, res) => {
+app.post("/deploy", async (req, res) => {
   const repoUrl = req.body.repoUrl;
   console.log(repoUrl);
   const id = generate();
--- src/utils.ts
+++ src/utils.ts
@@ -3,7 +3,7 @@ const MAX_LEN = 5;
 export function generate(){
     let id = "";
     const subset = "1234556asdfghjklqwertyuiop";
-    for(let i =0; i< length; i++){
+    for(let i =0; i< MAX_LEN; i++){
         id += subset[Math.floor(Math.random()* subset.length)];
     }
 
