dbf315ca0ead1c8330cd1a1d28a7ff83175fc012
Shikhar Bansal <shikharbansal007@gmail.com>
2024-03-30 22:47:23 +0530

    Extract all the files from github repo

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,9 @@ import cors from "cors";
 import { generate } from "./utils";
 import simpleGit from "simple-git";
 import bodyParser from "body-parser";
-
+import path from "path";
+import fs from "fs";
+import { getAllFiles } from "./file";
 
 const app = express();
 app.use(cors());
@@ -14,8 +16,17 @@ app.post("/deploy", async (req, res) => {
   console.log(repoUrl);
   const id = generate();
 
-  await simpleGit().clone(repoUrl, `output/${id}`);
+  await simpleGit().clone(repoUrl, path.join(__dirname, `output/${id}`));
 
+  const outputDir = path.join(__dirname, `output/${id}`);
+  if (fs.existsSync(outputDir)) {
+    const files = getAllFiles(outputDir);
+    //console.log(files);
+  } else {
+    console.error("Directory does not exist:", outputDir);
+  }
+  
+  // put this into S3
   res.json({
     id: id,
   });
